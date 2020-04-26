c705048b623b3a4bfe9cbb7690a067dde9458929
Lucas Oliveira <lucasoliveira.si@outlook.com>
2020-04-26 12:04:12 -0300

    adicionando navbar para logout e perfil de usuário

---
+++ src/app/modules/container/navbar/navbar.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserService } from 'src/app/shared/providers/user.service';
+import { ToastrService } from 'ngx-toastr';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent implements OnInit {
+  
+  public token: string;
+
+  constructor(
+    private router: Router,
+    private userService: UserService,
+    private toastr: ToastrService
+  ) { }
+
+  ngOnInit() {
+    this.getToken();
+  }
+
+  private getToken() {
+    this.token = localStorage.getItem('token');
+  }
+
+  public logout() {
+      (response: any) => {
+        if (response.code === 200) {
+            localStorage.removeItem('token');
+            this.token = undefined;
+            this.router.navigate(['/home']);
+            this.toastr.success('Deslogado com sucesso.', 'Successo');
+        }
+      }
+  }
+
+}
