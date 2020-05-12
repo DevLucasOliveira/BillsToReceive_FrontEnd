b8779ee95567c9e5ccf8ae2a05df3ad51e25d213
Lucas Oliveira <lucasoliveira.si@outlook.com>
2020-05-11 21:12:09 -0300

    Personalizando tela inicial

--- src/app/modules/container/container.module.ts
+++ src/app/modules/container/container.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from './footer/footer.component';
-import { HeaderComponent } from './header/header.component';
 import { InitialPageComponent } from './initial-page/initial-page.component';
 import { ContainerRoutingModule } from './container-routing.module';
 import { NavbarComponent } from './navbar/navbar.component';
@@ -9,7 +8,7 @@ import { NavbarComponent } from './navbar/navbar.component';
 
 
 @NgModule({
-  declarations: [FooterComponent, HeaderComponent, InitialPageComponent, NavbarComponent],
+  declarations: [FooterComponent, InitialPageComponent, NavbarComponent],
   imports: [
     CommonModule,
     ContainerRoutingModule
